76406acb35390f8d969b889704c75e7a3fe68121
tanghongchun <1164329425@qq.com>
2019-06-16 17:14:18 +0800

    wangwu提交

--- src/main/java/com/csii/bind/BindApplication.java
+++ src/main/java/com/csii/bind/BindApplication.java
@@ -5,6 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class BindApplication {
    public static void main(String[] args){
+       System.out.println("我是wangwu");
        SpringApplication.run(BindApplication.class,args);
    }
 
