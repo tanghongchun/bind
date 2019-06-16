707a62d79dc06f1957145ec73ce4b08a103dd8d0
tanghongchun <1164329425@qq.com>
2019-06-16 19:12:07 +0800

    vv

--- src/main/java/com/csii/bind/BindApplication.java
+++ src/main/java/com/csii/bind/BindApplication.java
@@ -13,7 +13,7 @@ public class BindApplication {
    public void getaa(){
        System.out.print("new ");
    }
-    public void delww2(){
+    public void delww6(){
         System.out.print(" ");
     }
 }
